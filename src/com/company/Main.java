7f35fd70252d0774cde70166bb6654ec077bb98c
unf1cs <unf1cs1@gmail.com>
2021-03-22 20:44:58 +0300

    test 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        int tolya = 12;
+//        int vasya = 8;
+//        if (tolya > vasya) {
+//            System.out.println("У Толи больше яблок");
+//        } else if (tolya == vasya) {
+//            System.out.println("Яблок поровну");
+//        } else {
+//            System.out.println("У Васи больше яблок");
+           for(int i=7;i>=1;i--){
+             System.out.println(i);
+           }
+
+            int num = 10;
+            int res = 0;
+          while (num<=10); {
+                res+= num;
+               num++;
+            }
+            System.out.println(res);
+
+
+        int[] mass = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int i=0;
+           while (i<=10);{
+               i++;
+            System.out.println(mass[i]);
+        }
+
+        }
+    }
