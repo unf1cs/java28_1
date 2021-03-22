26cbe386ad9da858333d8fb556ea6e06af8ee4ab
unf1cs <unf1cs1@gmail.com>
2021-03-22 21:12:12 +0300

    test 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,17 @@ public class Main {
             System.out.println(mass[x]);
             x++;
            }
+        System.out.println("Задание 3");
+        int n =12;
+        int[] f = new int[n];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        for (int i = 0; i < n; ++i) {
+            System.out.println(f[i]);
+        }
+
     }
 }
