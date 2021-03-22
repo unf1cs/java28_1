6048a8a478c13775702fe759661e83d7f666b75a
unf1cs <unf1cs1@gmail.com>
2021-03-22 21:14:42 +0300

    test 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("задание 1");
+        System.out.println("Вывести числа от 7 до 1 с помощью цикла for");
            for(int i=7;i>=1;i--){
              System.out.println(i);
            }
-        System.out.println("Задание 2");
+        System.out.println("Создать и проинициализировать массив из 10 любых чисел. Вывести все элементы массива на экран с помощью цикла while");
             int[] mass = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int x = 0;
         while (x<mass.length) {
             System.out.println(mass[x]);
             x++;
            }
-        System.out.println("Задание 3");
+        System.out.println("Написать алгоритм для вывода последовательности Фибоначчи с помощью цикла до 100");
         int n =12;
         int[] f = new int[n];
         f[0] = 0;
