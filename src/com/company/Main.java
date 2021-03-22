99b6af385d2867ee45aa69b3cfae158bc5c17efd
unf1cs <unf1cs1@gmail.com>
2021-03-22 21:00:14 +0300

    test 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,34 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-//        int tolya = 12;
-//        int vasya = 8;
-//        if (tolya > vasya) {
-//            System.out.println("У Толи больше яблок");
-//        } else if (tolya == vasya) {
-//            System.out.println("Яблок поровну");
-//        } else {
-//            System.out.println("У Васи больше яблок");
+        System.out.println("задание 1");
            for(int i=7;i>=1;i--){
              System.out.println(i);
            }
-
-            int num = 10;
-            int res = 0;
-          while (num<=10); {
-                res+= num;
-               num++;
-            }
-            System.out.println(res);
-
-
-        int[] mass = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        int i=0;
-           while (i<=10);{
-               i++;
-            System.out.println(mass[i]);
-        }
-
-        }
+        System.out.println("Задание 2");
+            int[] mass = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int x = 0;
+        while (x<mass.length) {
+            System.out.println(mass[x]);
+            x++;
+           }
     }
+}
